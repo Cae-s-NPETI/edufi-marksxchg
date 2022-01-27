6000b2d7f2ea7e0789806fb9bace9efd41f6f9e6
CaeNGnp <s10196427@connect.np.edu.sg>
2022-01-27 11:52:05 +0800

    trades: Optimize docker image size
    800MB -> 120MB (~85%)

--- trades/src/config.ts
+++ trades/src/config.ts
@@ -1,3 +1,3 @@
 export default {
-    REDIS_URL: `redis://default:${process.env.REDIS_PASS}@localhost:9163`
+    REDIS_URL: `redis://default:${process.env.REDIS_PASS}@${process.env.REDIS_HOST ?? "localhost:9163"}`
 }
